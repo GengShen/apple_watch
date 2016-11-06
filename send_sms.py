edb911f8e67d93a03c25ecc139622a827243321f
shennon <453992341@qq.com>
2016-11-06 23:29:06 +0800

    delete the key and mobile phone in the send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -3,7 +3,7 @@ import requests as r
 import urllib
 
 def sendsms(j,w,b):
-    u='http://utf8.sms.webchinese.cn/?Uid=aniu&Key=45b60b78c5ba94ca3aac&smsMob=15023078022&smsText='
+    u='http://utf8.sms.webchinese.cn/?Uid=aniu&Key=&smsMob=&smsText='#key and mobile phone number has been deleted
     text='7+黑色版预约存量： 解放碑：%s、 万象城：%s、 北城天街：%s'
     text = text%(j,w,b)
     text = urllib.quote(text)
